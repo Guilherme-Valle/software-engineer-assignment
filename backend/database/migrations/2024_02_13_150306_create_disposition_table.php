d348ca81be5b4769d04f3bfc811c83b0d491f8e2
Guilherme-Valle <guidovalle1608@gmail.com>
2024-02-13 12:26:49 -0300

    Create Laravel project and define migrations

---
+++ backend/database/migrations/2024_02_13_150306_create_disposition_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Candidate;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disposition', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Candidate::class);
+            $table->enum('disposition', ['undecided', 'hired', 'rejected']);
+            $table->enum('hire_type', ['internal', 'external'])->nullable();
+            $table->decimal('fee', $precision = 12, $scale = 2);
+            $table->text('currency')->nullable();
+            $table->text('rejection_reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disposition');
+    }
+};
